2905fe06d4f2385f98967d801ce6c57370b9f3ab
815380884 <815380884@qq.com>
2018-06-10 14:49:41 +0800

    Merge branch 'master' of E:\Android\Project\WanShaAPP-master\WanShaAPP with conflicts.

--- app/src/main/java/com/example/l/wanshaapp/fragment/jiemi_fragment.java
+++ app/src/main/java/com/example/l/wanshaapp/fragment/jiemi_fragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
+import com.example.l.wanshaapp.fenleiresource.jiemiresource;
 
 import com.example.l.wanshaapp.R;
 import com.example.l.wanshaapp.adapter.jiemiAdapter;
@@ -36,11 +37,9 @@ public class jiemi_fragment extends Fragment {
     }
 
     private void DataListtest() {
+
         dataList = new ArrayList<Map<String, Object>>();
-        String string[] = {"http://p2.so.qhimgs1.com/bdr/_240_/t01f61dbbc072bac262.jpg","https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=4050523606,2170310969&fm=27&gp=0.jpg",
-                "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=862591842,2864954084&fm=27&gp=0.jpg","https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2248907317,2456606936&fm=27&gp=0.jpg","http://p2.so.qhimgs1.com/bdr/_240_/t01f61dbbc072bac262.jpg","https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=4050523606,2170310969&fm=27&gp=0.jpg",
-                "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=862591842,2864954084&fm=27&gp=0.jpg","https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2248907317,2456606936&fm=27&gp=0.jpg","http://p2.so.qhimgs1.com/bdr/_240_/t01f61dbbc072bac262.jpg","https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=4050523606,2170310969&fm=27&gp=0.jpg",
-                "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=862591842,2864954084&fm=27&gp=0.jpg","https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2248907317,2456606936&fm=27&gp=0.jpg"};
+
 
        String youximing[]={"创世战记","爆炸火花","香蕉足球","爱的狂想曲","小屁孩儿","来生再会","萤火之森","你知道吗","蓝色光头","人小鬼大"};
 
