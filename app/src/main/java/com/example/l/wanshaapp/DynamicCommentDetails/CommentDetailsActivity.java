096ab6881e8cb7e5dbdd198c753780495296bf6c
wangxian01 <37919493+wangxian01@users.noreply.github.com>
2018-06-13 23:54:01 +0800

    调整精选评论界面

--- app/src/main/java/com/example/l/wanshaapp/DynamicCommentDetails/CommentDetailsActivity.java
+++ app/src/main/java/com/example/l/wanshaapp/DynamicCommentDetails/CommentDetailsActivity.java
@@ -1,22 +1,34 @@
 package com.example.l.wanshaapp.DynamicCommentDetails;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
+import com.example.l.wanshaapp.DyFocusCommentDetails.FcousCommentActivity;
+import com.example.l.wanshaapp.DynamicChoiceness.BeanChoiceness;
 import com.example.l.wanshaapp.DynamicChoiceness.SerializableMap;
 import com.example.l.wanshaapp.DynamicHome.CircleImageView;
 import com.example.l.wanshaapp.R;
@@ -30,49 +42,43 @@ import java.util.Map;
 import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
 
 public class CommentDetailsActivity extends AppCompatActivity {
-    private Toolbar toolbar;
     private JCVideoPlayerStandard mCommentViodeoview;
     private List<Map<String,Object>> dataList;
     private ListView mCommentsChoicenessMain;
-    private ScrollView mCommentScroll;
     private CircleImageView mDetailPageUserLogo;
     private TextView mDetailPageUserName;
     private TextView mDetailPageTime;
     private ImageView mDetailPageFocus;
     private TextView mCommentTextMain;
-
-//    private RecyclerView mCommentsRecyclerview;
-
+    private TextView mChoicenessCommentsPl;
+    private BottomSheetDialog dialog;
+    private TextView mChoicenessCommentsTextUnfold;
+    private AdapterCommentMain adapterCommentMain;
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment_details);
-        mDetailPageUserLogo = (CircleImageView) findViewById(R.id.detail_page_userLogo);
-        mDetailPageUserName = (TextView) findViewById(R.id.detail_page_userName);
-        mDetailPageTime = (TextView) findViewById(R.id.detail_page_time);
-        mDetailPageFocus = (ImageView) findViewById(R.id.detail_page_focus);
-        mCommentTextMain = (TextView) findViewById(R.id.comment_text_main);
-       // mCommentScroll = (ScrollView) findViewById(R.id.comment_scroll);
-        toolbar = findViewById(R.id.toolbar);
-        mCommentsChoicenessMain = (ListView) findViewById(R.id.comments_choiceness_main);
-//        mCommentsRecyclerview = (RecyclerView) findViewById(R.id.comments_recyclerview);
-        setSupportActionBar(toolbar);
-        //设置显示返回按钮
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        CollapsingToolbarLayout collapsingToolbar =
-                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        //设置标题为空
-        collapsingToolbar.setTitle(" ");
 
+        View headView = getLayoutInflater().inflate(R.layout.headview_choiceness_comment, null);
+
+
+        mChoicenessCommentsTextUnfold = (TextView)headView.findViewById(R.id.choiceness_comments_text_unfold);
+        mDetailPageUserLogo = (CircleImageView) headView.findViewById(R.id.detail_page_userLogo);
+        mDetailPageUserName = (TextView)headView. findViewById(R.id.detail_page_userName);
+        mDetailPageTime = (TextView) headView.findViewById(R.id.detail_page_time);
+        mDetailPageFocus = (ImageView) headView.findViewById(R.id.detail_page_focus);
+        mCommentTextMain = (TextView)headView. findViewById(R.id.comment_text_main);
+        mCommentsChoicenessMain = (ListView)findViewById(R.id.comments_choiceness_main);
+        mChoicenessCommentsPl = (TextView) findViewById(R.id.choiceness_comments_pl);
         //传递参数
         Bundle bundle = getIntent().getExtras();
         SerializableMap serializableMap = (SerializableMap) bundle.get("map");
 
-        mCommentViodeoview = (JCVideoPlayerStandard) findViewById(R.id.comment_viodeoview);
+        mCommentViodeoview = (JCVideoPlayerStandard) headView.findViewById(R.id.comment_viodeoview);
         //设置网络视频
         mCommentViodeoview.setUp(String.valueOf(serializableMap.getMap("map").get("ChoicenessViodeoview"))
-                , JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "\t\t\t\t\t\t"+String.valueOf(serializableMap.getMap("map").get("UpIntroduce")));
+                , JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "\t\t"+String.valueOf(serializableMap.getMap("map").get("UpIntroduce")));
 
         //设置视频缩略图
         Picasso.with(this)
@@ -89,48 +95,49 @@ public class CommentDetailsActivity extends AppCompatActivity {
         mDetailPageTime.setText(String.valueOf(serializableMap.getMap("map").get("ChoicenessUpTime")));
         //获取游戏介绍文字
         mCommentTextMain.setText(String.valueOf(serializableMap.getMap("map").get("ChoicenessUpText")));
+
+
+        /**
+         * 设置收起和展开状态
+         */
+        final BeanChoiceness beanChoiceness = new BeanChoiceness();
+        beanChoiceness.setunfold(false);
+
+        mChoicenessCommentsTextUnfold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean flag = beanChoiceness.getunfold();
+                if (flag){
+                    mCommentTextMain.setMaxLines(2);
+                    mChoicenessCommentsTextUnfold.setText("展开");
+                    mChoicenessCommentsTextUnfold.setTextColor(Color.parseColor("#464646"));
+                    beanChoiceness.setunfold(false);
+                }else{
+                    mCommentTextMain.setMaxLines(1000);
+                    mChoicenessCommentsTextUnfold.setText("收起");
+                    mChoicenessCommentsTextUnfold.setTextColor(Color.parseColor("#1296DB"));
+                    beanChoiceness.setunfold(true);
+                }
+            }
+        });
+         /*
+        * 点击评论
+        * */
+        mChoicenessCommentsPl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCommentDialog();
+            }
+        });
+
         initDataList();
-       // setListViewHeightBasedOnChildren();
-        //添加适配器
-        AdapterCommentMain adapterCommentMain = new AdapterCommentMain(this, dataList, R.layout.item_main_comment);
+        //头布局添加适配器
+        mCommentsChoicenessMain.addHeaderView(headView);
+       adapterCommentMain = new AdapterCommentMain(this, dataList, R.layout.item_main_comment);
         mCommentsChoicenessMain.setAdapter(adapterCommentMain);
-//
-//        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(this,dataList);
-//        //mCommentsRecyclerview.setHasFixedSize(false);
-//        mCommentsRecyclerview.setAdapter(recyclerViewAdapter);
-//        mCommentsRecyclerview.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
-//        //设置分隔线
-//        mCommentsRecyclerview.addItemDecoration( new DividerItemDecoration(this,LinearLayoutManager.VERTICAL));
-//        //mCommentsRecyclerview.setNestedScrollingEnabled(false);
 
-
-        //重新调整ScrollView的位置
-//        mCommentScroll.post(new Runnable() {
-//            @Override
-//            public void run() {
-//                mCommentScroll.scrollTo(0,0);
-//            }
-//        });
     }
 
-    public void setListViewHeightBasedOnChildren(ListView listView) {
-        ListAdapter listAdapter = listView.getAdapter();
-        if (listAdapter == null) {
-            return;
-        }
-
-        int totalHeight = 0;
-        for (int i = 0; i < listAdapter.getCount(); i++) {
-            View listItem = listAdapter.getView(i, null, listView);
-            listItem.measure(0, 0);  // 获取item高度
-            totalHeight += listItem.getMeasuredHeight();
-        }
-
-        ViewGroup.LayoutParams params = listView.getLayoutParams();
-        // 最后再加上分割线的高度和padding高度，否则显示不完整。
-        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1))+listView.getPaddingTop()+listView.getPaddingBottom();
-        listView.setLayoutParams(params);
-    }
 
     /**
      * 初始化适配器需要的数据格式
@@ -141,24 +148,74 @@ public class CommentDetailsActivity extends AppCompatActivity {
             Map<String, Object> map = new HashMap<String, Object>();
 
             map.put("UpId", "游客甲"+i);
-
+            map.put("CommentText","评论内容"+i);
             dataList.add(map);
         }
 
     }
 
+    /**
+     * by moos on 2018/06
+     * func:弹出评论框
+     */
+    private void showCommentDialog(){
+        dialog = new BottomSheetDialog(this);
+        final View commentView = LayoutInflater.from(this).inflate(R.layout.comment_dialog_layout,null);
+        final EditText commentText = (EditText) commentView.findViewById(R.id.dialog_comment_et);
+        final Button bt_comment = (Button) commentView.findViewById(R.id.dialog_comment_bt);
+        dialog.setContentView(commentView);
+        /**
+         * 解决bsd显示不全的情况
+         */
+        View parent = (View) commentView.getParent();
+        BottomSheetBehavior behavior = BottomSheetBehavior.from(parent);
+        commentView.measure(0,0);
+        behavior.setPeekHeight(commentView.getMeasuredHeight());
 
-    //添加返回按钮点击事件
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // TODO Auto-generated method stub
+        bt_comment.setOnClickListener(new View.OnClickListener() {
 
-        //android.R.id.home对应应用程序图标的id
-        if(item.getItemId() == android.R.id.home)
-        {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+            @Override
+            public void onClick(View view) {
+                String commentContent = commentText.getText().toString().trim();
+                if(!TextUtils.isEmpty(commentContent)){
+                    dialog.dismiss();
+                    //dataList = new ArrayList<Map<String, Object>>();
+                    Map<String, Object> map = new HashMap<String, Object>();
+
+                    map.put("UpId", "谭林");
+                    map.put("CommentText",commentContent);
+                    // dataList.add(map);
+                    adapterCommentMain.addTheCommentData(map);
+                    //mFocusCommentsMainlist.smoothScrollToPosition(0);
+                    mCommentsChoicenessMain.deferNotifyDataSetChanged();
+                    adapterCommentMain.notifyDataSetChanged();
+                    Toast.makeText(CommentDetailsActivity.this,"评论成功",Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(CommentDetailsActivity.this,"评论内容不能为空",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        commentText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!TextUtils.isEmpty(charSequence) && charSequence.length()>2){
+                    bt_comment.setBackgroundColor(Color.parseColor("#FFB568"));
+                }else {
+                    bt_comment.setBackgroundColor(Color.parseColor("#D8D8D8"));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        dialog.show();
     }
 }
