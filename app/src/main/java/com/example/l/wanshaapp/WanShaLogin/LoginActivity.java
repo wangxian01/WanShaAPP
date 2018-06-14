cbd8c934c917dd5b5976edbc7c1bf34c3a59f241
tanlin666 <70974992@qq.com>
2018-06-14 19:36:53 +0800

    狠毒功能提交

--- app/src/main/java/com/example/l/wanshaapp/WanShaLogin/LoginActivity.java
+++ app/src/main/java/com/example/l/wanshaapp/WanShaLogin/LoginActivity.java
@@ -16,7 +16,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.l.wanshaapp.HomeBannerActivity;
 import com.example.l.wanshaapp.R;
 import com.squareup.okhttp.Request;
 import com.zhy.http.okhttp.OkHttpUtils;
